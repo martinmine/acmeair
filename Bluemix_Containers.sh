fd35202d705560fc47e9c99f84141d0b20421fd8
Joe McClure <jdmcclur@us.ibm.com>
2016-06-14 14:01:58 -0500

    Add main service for Bluemix

--- Bluemix_Containers.sh
+++ Bluemix_Containers.sh
@@ -1,24 +1,30 @@
 REGISTRY=registry.ng.bluemix.net
-NAME_SPACE=strong
-MONGO_BRIDGE=MongoBridge
+NAME_SPACE=
+MONGO_BRIDGE=
 SD_URL=https://servicediscovery.ng.bluemix.net
-SD_TOKEN=1m3rliolucbampleoq36am82bdfv0othuruoefe6enop27ab7cnp
+SD_TOKEN=
 
-docker build -f ./acmeair-as/Dockerfile_BlueMix_as -t ${REGISTRY}/${NAME_SPACE}/acmeair_authservice .
-docker build -f ./acmeair-bs/Dockerfile_BlueMix_bs -t ${REGISTRY}/${NAME_SPACE}/acmeair_bookingservice .
-docker build -f ./acmeair-cs/Dockerfile_BlueMix_cs -t ${REGISTRY}/${NAME_SPACE}/acmeair_customerservice .
-docker build -f ./acmeair-fs/Dockerfile_BlueMix_fs -t ${REGISTRY}/${NAME_SPACE}/acmeair_flightservice .
-
-#docker push ${REGISTRY}/${NAME_SPACE}/acmeair_mainservice
-docker push ${REGISTRY}/${NAME_SPACE}/acmeair_authservice
-docker push ${REGISTRY}/${NAME_SPACE}/acmeair_bookingservice
-docker push ${REGISTRY}/${NAME_SPACE}/acmeair_customerservice
-docker push ${REGISTRY}/${NAME_SPACE}/acmeair_flightservice
-
-#cf ic run -e SERVICE_NAME=main -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name main_1 ${REGISTRY}/${NAME_SPACE}/acmeair_mainservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name Jauth_1     ${REGISTRY}/${NAME_SPACE}/acmeair_authservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name Jbooking_1  ${REGISTRY}/${NAME_SPACE}/acmeair_bookingservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name Jcustomer_1 ${REGISTRY}/${NAME_SPACE}/acmeair_customerservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name Jflight_1   ${REGISTRY}/${NAME_SPACE}/acmeair_flightservice
+docker build -f ./acmeair-mainapp/Dockerfile_BlueMix_main -t ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice .
+docker build -f ./acmeair-as/Dockerfile_BlueMix_as -t ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice .
+docker build -f ./acmeair-bs/Dockerfile_BlueMix_bs -t ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice .
+docker build -f ./acmeair-cs/Dockerfile_BlueMix_cs -t ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice .
+docker build -f ./acmeair-fs/Dockerfile_BlueMix_fs -t ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice .
 
 
+docker tag  -f acmeair_java_mainservice:latest ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice:latest
+docker tag -f acmeair_java_authservice:latest ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice:latest
+docker tag -f acmeair_java_bookingservice:latest ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice:latest
+docker tag -f acmeair_java_customerservice:latest ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice:latest
+docker tag -f acmeair_java_flightservice:latest ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice:latest
+
+docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice
+docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice
+docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice
+docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice
+docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice
+
+cf ic run -e SERVICE_NAME=main -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name main_2 ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice
+cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=auth     -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name auth_2     ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice
+cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=booking  -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name booking_2  ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice
+cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=customer -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name customer_2 ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice
+cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=flight   -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name flight_2   ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice
