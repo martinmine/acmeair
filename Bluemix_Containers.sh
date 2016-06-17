d62381ae5757709eac977bd99bd0f402312360e8
Joe McClure <jdmcclur@us.ibm.com>
2016-06-17 15:44:15 -0500

    Some small perf improvements

--- Bluemix_Containers.sh
+++ Bluemix_Containers.sh
@@ -10,8 +10,7 @@ docker build -f ./acmeair-bs/Dockerfile_BlueMix_bs -t acmeair_java_bookingservic
 docker build -f ./acmeair-cs/Dockerfile_BlueMix_cs -t acmeair_java_customerservice .
 docker build -f ./acmeair-fs/Dockerfile_BlueMix_fs -t acmeair_java_flightservice .
 
-
-docker tag  -f acmeair_java_mainservice ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice:latest
+docker tag -f acmeair_java_mainservice ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice:latest
 docker tag -f acmeair_java_authservice ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice:latest
 docker tag -f acmeair_java_bookingservice ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice:latest
 docker tag -f acmeair_java_customerservice ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice:latest
@@ -23,10 +22,8 @@ docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice
 docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice
 docker push ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice
 
-
-
 cf ic run -e SERVICE_NAME=main -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name main_java_1 ${REGISTRY}/${NAME_SPACE}/acmeair_java_mainservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=auth     -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name auth_java_1     ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=booking  -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name booking_java_1  ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=customer -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name customer_java_1 ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice
-cf ic run -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=flight   -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name flight_java_1   ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice
+cf ic run -m 256 -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=auth     -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name auth_java_1     ${REGISTRY}/${NAME_SPACE}/acmeair_java_authservice
+cf ic run -m 256 -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=booking  -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name booking_java_1  ${REGISTRY}/${NAME_SPACE}/acmeair_java_bookingservice
+cf ic run -m 256 -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=customer -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name customer_java_1 ${REGISTRY}/${NAME_SPACE}/acmeair_java_customerservice
+cf ic run -m 256 -e CCS_BIND_APP=${MONGO_BRIDGE} -e SERVICE_NAME=flight   -e SD_URL=${SD_URL} -e SD_TOKEN=${SD_TOKEN} --name flight_java_1   ${REGISTRY}/${NAME_SPACE}/acmeair_java_flightservice
