89a7e90bfd98e2f9f239e12510601e931a8a6e26
Moss Uchida <super@192.168.0.13>
2016-07-11 17:09:26 -0400

    Adding DB bridge binding

--- acmeair-webapp/Bluemix_Mono.sh
+++ acmeair-webapp/Bluemix_Mono.sh
@@ -1,8 +1,9 @@
 REGISTRY=registry.ng.bluemix.net
 NAME_SPACE=strong
+MONGO_BRIDGE=MongoBridge
 
 docker build -f ./Dockerfile_BlueMix_webapp -t ${REGISTRY}/${NAME_SPACE}/acmeair_webapp .
 docker push ${REGISTRY}/${NAME_SPACE}/acmeair_webapp
-cf ic run --name webapp_1   ${REGISTRY}/${NAME_SPACE}/acmeair_webapp
+cf ic run --name webapp_1 -e CCS_BIND_APP=${MONGO_BRIDGE} ${REGISTRY}/${NAME_SPACE}/acmeair_webapp
 
 
