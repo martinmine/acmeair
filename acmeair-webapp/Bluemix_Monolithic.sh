64f4302d25a45ada18b598a91a16829ac9587c79
Moss Uchida <super@192.168.0.13>
2016-07-12 11:22:39 -0400

    Minor name change

--- acmeair-webapp/Bluemix_Monolithic.sh
+++ acmeair-webapp/Bluemix_Monolithic.sh
@@ -4,6 +4,6 @@ MONGO_BRIDGE=MongoBridge
 
 docker build -f ./Dockerfile_BlueMix_webapp -t ${REGISTRY}/${NAME_SPACE}/acmeair_webapp .
 docker push ${REGISTRY}/${NAME_SPACE}/acmeair_webapp
-cf ic run --name webapp_1 -e CCS_BIND_APP=${MONGO_BRIDGE} ${REGISTRY}/${NAME_SPACE}/acmeair_webapp
+cf ic run --name Jmonolithic_1 -e CCS_BIND_APP=${MONGO_BRIDGE} ${REGISTRY}/${NAME_SPACE}/acmeair_webapp
 
 
